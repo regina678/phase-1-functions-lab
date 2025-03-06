9bcb3ad392823efc6f4b9b256aeed729bd850f1f
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-06 14:35:40 +0300

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+// 1 block = 264 feet
+const hqBlock = 42;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - hqBlock);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far'; 
+    }
+}
